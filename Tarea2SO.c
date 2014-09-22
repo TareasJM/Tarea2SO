42f491e9fb34277ac42ef895a796c6d1b5f6c578
Jose Castro <n3ggro@gmail.com>
2014-09-22 17:22:09 -0300

    dejamos la cagá

--- Tarea2SO.c
+++ Tarea2SO.c
@@ -5,18 +5,24 @@
 #include <errno.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 #include <stdlib.h>
 
 //Tamaño y nombre de memoria compartida
-#define SHMSZ 368 // 2 ints + 6 strings de 60 chars c/u
+#define SHMSZ 256 // 1 string de 256 chars
 #define SHMK 1234 // nombre random
 
 void upTime();
 void RamInfo();
 void ProcsInfo();
- 
+int cantWrite(int shmid);
+int cantRead(int shmid);
+void readSHM(int shmid);
+void writeSHM(int shmid, char* string);
+
 int main (int argc, char const *argv[])
 { 
+
   // Verificación de argumentos
   if (argc != 3)
   {
@@ -28,6 +34,7 @@ int main (int argc, char const *argv[])
   sscanf(argv[2], "%d", &wait);
 
   int shmid;
+  char *msg;
 
   // pide la memoria compartida
   if ((shmid = shmget(SHMK, SHMSZ, IPC_CREAT | 0666)) < 0)
@@ -35,8 +42,16 @@ int main (int argc, char const *argv[])
 	  printf("Error: shmget\n");
 	  return 1;
   }
-  
-  // analiza el sistema rep veces
+
+  if ((msg = shmat(shmid, NULL, 0)) == (char *) -1)
+  {
+	  printf("Error: shmat\n");
+	  return 1;
+  }
+
+  *msg++ = '0';
+  *msg-- = '\0';
+
 	// primer fork
 	pid_t pid = fork();
 	if(pid == -1)
@@ -51,11 +66,12 @@ int main (int argc, char const *argv[])
 		while(r-- > 0)
 		{	
 			// analiza
-		  upTime();		
+		  upTime(shmid, getpid());		
 		  // espera wait segundos
 			if(r > 0)
 				sleep(wait);
 		}
+		while(cantWrite(shmid));
 	  printf("%d: SubProceso finalizado.\n", getpid());
 		exit(0);
 	}
@@ -71,14 +87,16 @@ int main (int argc, char const *argv[])
 	if(pid == 0)
 	{
 		int r = rep;
+		sleep(5);
 		while(r-- > 0)
 		{	
 			// analiza
-		  RamInfo();
+		  RamInfo(shmid, getpid());
 		  // espera wait segundos
 			if(r > 0)
 				sleep(wait);
 		}
+		while(cantWrite(shmid));
 	  printf("%d: SubProceso finalizado.\n", getpid());
 		exit(0);
 	}
@@ -93,21 +111,29 @@ int main (int argc, char const *argv[])
 	}
 	if(pid == 0)
 	{
+		sleep(10);
 		int r = rep;
 		while(r-- > 0)
 		{	
 			// analiza
-		  ProcsInfo();
+		  ProcsInfo(shmid, getpid());
 		  // espera wait segundos
 			if(r > 0)
 				sleep(wait);
 		}
+		while(cantWrite(shmid));
 	  printf("%d: SubProceso finalizado.\n", getpid());
 		exit(0); 
 	}     
 
   // fin del proceso padre
-  sleep(wait*rep);
+  int i = (3*rep) + 3;
+  while(i-- > 0)
+  {
+  	while(cantRead(shmid))
+  		sleep(1);
+  	readSHM(shmid);
+  }
   printf("%d: Programa finalizado\n", getpid());
 
   return 0;
@@ -115,33 +141,115 @@ int main (int argc, char const *argv[])
 }
 
 //Hace fork y escribe el tiempo del sistema
-void upTime()
+void upTime(int shmid, int pid)
 {
 	struct sysinfo info;
 	sysinfo(&info);
-  printf("%d: El sistema lleva %lus encendido.\n", getpid(), info.uptime);
+	while(cantWrite(shmid));
+	char *msg = (char *)malloc(sizeof(char)*256);
+  sprintf(msg, "%d: El sistema lleva %lus encendido.\n", pid, info.uptime);
+	writeSHM(shmid, msg);
+	free(msg);
   return;
 }
 
 //Hace fork y escribe la información de la RAM del sistema
-void RamInfo()
+void RamInfo(int shmid, int pid)
 {
 	struct sysinfo info;
 	sysinfo(&info);
-  printf("%d: Disponibles %luMB de %luMB.\n", getpid(), (info.freeram)/1048576, (info.totalram)/1048576);
+	while(cantWrite(shmid));
+  char *msg = (char *)malloc(sizeof(char)*256);
+  sprintf(msg, "%d: Disponibles %luMB de %luMB.\n", pid, (info.freeram)/1048576, (info.totalram)/1048576);
+	writeSHM(shmid, msg);
+	free(msg);
   return;
 }
 
 //Hace fork y escribe la información de Procesos del sistema
-void ProcsInfo()
+void ProcsInfo(int shmid, int pid)
 {
 	struct sysinfo info;
 	sysinfo(&info);
-  printf("%d: Hay %d procesos en ejecucion.\n", getpid(), info.procs);
+	while(cantWrite(shmid));
+  char *msg = (char *)malloc(sizeof(char)*256);
+  sprintf(msg, "%d: Hay %d procesos en ejecucion.\n", pid, info.procs);
+	writeSHM(shmid, msg);
+	free(msg);
   return;
 }
 
-void writeSHM(char* string)
+// Revisa si los procesos hijos pueden escribir en la memoria compartida
+int cantWrite(int shmid)
 {
+	char *msg;
+	if ((msg = shmat(shmid, NULL, 0)) == (char *) -1)
+  {
+	  printf("Error: shmat en write\n");
+	  return 1;
+  }
 
+  if (*msg == '1')
+  {
+  	return 1;
+  }
+  // aqui se activa el semaforo
+	return 0;
+}
+
+// Revisa si el padre tiene mensajes para leer en la memoria compartida
+int cantRead(int shmid)
+{
+	char *msg;
+	if ((msg = shmat(shmid, NULL, 0)) == (char *) -1)
+  {
+	  printf("Error: shmat\n");
+	  return 1;
+  }
+
+  if (*msg == '0')
+  {
+  	return 1;
+  }
+  // aqui se activa el semaforo
+  return 0;
+
+}
+
+void readSHM(int shmid)
+{
+	char *msg;
+	if ((msg = shmat(shmid, NULL, 0)) == (char *) -1)
+  {
+	  printf("Error: shmat\n");
+	  exit(1);
+  }
+  printf("shm: %s\n", msg);
+  FILE *p;
+  p = fopen("log.txt","a+");
+
+  *msg++ = '0';
+
+  fprintf(p, "%s\n", msg);
+
+  fclose(p);
+  
+}
+
+void writeSHM(int shmid, char* string)
+{
+	char *msg;
+	if ((msg = shmat(shmid, NULL, 0)) == (char *) -1)
+  {
+	  printf("Error: shmat\n");
+	  exit(1);
+  }
+
+  *msg++ = '1';
+
+  while(*string != '\0')
+  {
+  	*msg++ = *string++;
+  }
+  *msg = '\0';
 }
